1b8e7a31ed7cf4cbc0c6eeb5387743d166bb973b
rmedcraft <rwtmedcraft@gmail.com>
2024-11-09 14:19:03 -0500

    home page & class management css update

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
     return (
-        <>
-        
-        </>
+        <div className="loginContainer">
+            <h1 style={{ textAlign: "center" }}>Enter your question here: </h1>
+            <textarea className="question"></textarea>
+            <button>Submit</button>
+        </div>
     )
 }
