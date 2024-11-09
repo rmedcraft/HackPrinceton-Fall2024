0d18db3b8be0cacc57042c9d3510de7d75b741a6
rmedcraft <rwtmedcraft@gmail.com>
2024-11-09 03:41:46 -0500

    some silly animatinos

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -3,12 +3,7 @@ import { Link } from "react-router-dom";
 export default function Login() {
     return (
         <>
-            <Link to={"/"}><h2 style={{
-                position: "absolute",
-                left: "5px",
-                top: "5px",
-                margin: "0px"
-            }}><i className="bi bi-arrow-left-square-fill"></i></h2></Link>
+            <Link to={"/"}><h2 className="backButton"><i className="bi bi-arrow-left-square-fill"></i></h2></Link>
 
             <div className="loginContainer">
                 <h2>Login: </h2>
--- src/Pages/Start.tsx
+++ src/Pages/Start.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Start() {
     return (
-        <>
-            <div className="homeImg"></div>
+        <div className="startContainer">
+            <div className="startImg"></div>
             <h1 className="title" style={{
                 textAlign: "center"
             }}>Welcome to !NAME PENDING!</h1>
@@ -15,6 +15,6 @@ export default function Start() {
                 <Link to="/login"><button>Login</button></Link>
                 <Link to="/signup"><button>Sign Up</button></Link>
             </div>
-        </>
+        </div>
     )
 }
