a4122a19546def9c28c4119ee61ce8f9ff271d05
rmedcraft <rwtmedcraft@gmail.com>
2024-11-09 13:12:54 -0500

    not working signup

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -5,24 +5,39 @@ import { useState, useEffect } from "react";
 export default function Login() {
     const [message, setMessage] = useState('');
 
+    type loginInfo = {
+        username: string,
+        password: string
+    }
+
+    const [login, setLogin] = useState({ username: "", password: "" })
+
 
     const fetchAPI = async () => {
         const response = await axios.get("http://localhost:3000/api/test")
         setMessage(JSON.stringify(response.data))
-
     }
     useEffect(() => {
         fetchAPI()
-        // fetch('/api/test', {
-        //     "method": "GET",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     }
-        // })
-        //     .then((response) => response.json())
-        //     .then((data) => setMessage(data.message))
-        //     .catch((error) => console.error('Error fetching data:', error));
+
     }, [message]);
+
+    function sendData() {
+        console.log("did something")
+
+        fetch('http://localhost:3000/api/signup', {
+            "method": "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(login)
+        })
+            .then((response) => {
+                response.json()
+            })
+            .then((data) => console.log("Success:", JSON.stringify(data)))
+            .catch((error) => console.error('Error fetching data:', error));
+    }
 
     return (
         <>
@@ -32,11 +47,11 @@ export default function Login() {
                 <h2>Login: </h2>
                 <div className="loginFlex">
                     <label>Username: </label>
-                    <input></input>
+                    <input onChange={(evt) => setLogin({ username: evt.target.value, password: login.password })}></input>
                     <label>Password: </label>
-                    <input type="password"></input>
+                    <input type="password" onChange={(evt) => ({ username: login.username, password: evt.target.value })}></input>
                 </div>
-                <button>Login!</button>
+                <button onClick={sendData}>Login!</button>
                 {/* onClick -> check if the username & password exists in the database, if it does bring them to the home page,
                  otherwise tell them to log in again*/}
                 <h1>{message}</h1>
